625ff60fe3724fe3f1a32e22d8b0cff29527ca9d
singh13janu <126705994+singh13janu@users.noreply.github.com>
2023-05-17 13:07:33 +0530

    Add files via upload
    initial commit

---
+++ java/com/example/birdhunt_game/GameActivity.java
@@ -0,0 +1,33 @@
+package com.example.birdhunt_game;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    MediaPlayer mp_bg;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mp_bg =MediaPlayer.create(this,R.raw.bg_music);
+        if(mp_bg!=null)
+        {
+            mp_bg.start();
+            mp_bg.setLooping(true);
+        }
+        setContentView(new GameView(this));
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mp_bg!=null)
+        {
+            mp_bg.stop();
+            mp_bg.release();
+        }
+    }
+}
